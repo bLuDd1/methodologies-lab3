7c14981d6311a094d2ae61ad047252d1d38cd5a7
bLuDd1 <dtavluj@gmail.com>
2023-05-30 21:30:36 +0300

    Added matrix endpoint and changed Dockerfile

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World! My name is Dmytro'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+
+    result = np.dot(matrix_a, matrix_b)
+
+    response = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "response": result.tolist()
+    }
+
+    return response
